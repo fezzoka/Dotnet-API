6c040b1f1f2588cc09f8549c70d8edca82f7a988
FELIPE TADEU MARICOTA <felipe.tmaricota@senacsp.edu.br>
2022-09-01 20:39:46 -0300

    Criação Controller e client Repo

---
+++ CadastroCLientes/Repository/ClienteRepository.cs
@@ -0,0 +1,49 @@
+﻿using CadastroCLientes.Data;
+using CadastroCLientes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroCLientes.Repository
+{
+    public class ClienteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ClienteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Cliente>> GetClientes()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+        public async Task<Cliente> GetClienteById(int id)
+        {
+                       return await _context.Clientes.FirstOrDefaultAsync(x => x.Id == id);         
+        }
+            public async Task<Cliente> AddCliente(Cliente cliente)
+            {
+                await _context.Clientes.AddAsync(cliente);
+                await _context.SaveChangesAsync();
+                return cliente;
+            }
+
+            public async Task<Cliente> UpdateCliente(Cliente clienteAlterado)
+            {
+                _context.Entry(clienteAlterado).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            return clienteAlterado;
+            }
+
+        public async void DeleteCliente(int id)
+        {
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Id == id);
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
